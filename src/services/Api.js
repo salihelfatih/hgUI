e5384c8f0600da20f8ea25afcc6ee98b5822965e
Salih <salihelobeid@gmail.com>
2022-12-02 18:50:28 -0500

    big changes

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
+
+let API_URL = "https://homemade-goodies.herokuapp.com";
+if (window.location.hostname === "localhost") {
+  API_URL = "http://localhost:5000";
+}
 
 export default () => {
+  // get dataAPI() {
   return axios.create({
-    baseURL: `http://localhost:5000`
-  })
-}
+    baseURL: API_URL,
+    // withCredentials: false,
+    headers: {
+      // Accept: 'application/json',
+      "Content-Type": "application/json",
+    },
+  });
+  // }
+};
